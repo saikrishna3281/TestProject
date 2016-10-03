430575a5100705d4080e198d73622e997d026cdf
vpalagummi <saikrishnasharma2010@gmail.com>
2016-10-03 14:45:09 +0530

    initial project version

---
+++ pythonApp/pythonApp.py
@@ -0,0 +1,14 @@
+﻿import pyodbc
+import sys
+
+try:
+    connection = pyodbc.connect('Driver={SQL Server};Server=VPALAGUMMIPC;Database=class;Trusted_Connection=yes;')
+    cursor = connection.cursor()
+    print "Successfully connected"
+    SQLCommand = "exec InsertData @Name = 'Minni',@Age=21,@Branch='CSE',@Gender='F'"
+    cursor.execute(SQLCommand)
+    cursor.close()
+    connection.commit()
+    connection.close() 
+except:
+    print "Unexpected error",sys.exc_info()[0]
